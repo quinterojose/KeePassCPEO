e39b350f8cf3493faedda68f957c2d523a6bbbba
Jose Quintero <jose.quintero@outlook.com>
2016-09-16 23:07:58 -0400

    Missing bunch of solution and project files from source control

---
+++ KeePassCPEO/CustomDateOptionsDialog.cs
@@ -0,0 +1,92 @@
+﻿using KeePass.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace KeePassCPEO
+{
+    /// <summary>
+    /// Implement a form to display the list of custom options and allow the user to edit the list.
+    /// </summary>
+    public partial class CustomDateOptionsDialog : Form
+    {
+        /// <summary>
+        /// Gets the new list of custom date options.
+        /// </summary>
+        public List<CustomDateOption> CustomDateOptions { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of CustomDateOptionsDialog
+        /// </summary>
+        /// <param name="customDateOptions">The list of custom date options.</param>
+        public CustomDateOptionsDialog(List<CustomDateOption> customDateOptions)
+        {
+            CustomDateOptions = customDateOptions;
+
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Execute when dialog is loaded.
+        /// </summary>
+        private void CustomOptionsDialog_Load(object sender, EventArgs e)
+        {
+            GlobalWindowManager.AddWindow(this);
+            BannerImage.Image = BannerFactory.CreateBanner(BannerImage.Width, BannerImage.Height, BannerStyle.Default, Properties.Resources.Date_48x48, Text, "Custom password expiration options");
+            CustomDateOptions.ForEach(o => CustomOptionsListBox.Items.Add(o));
+        }
+
+        /// <summary>
+        /// Execute when dialog is closed.
+        /// </summary>
+        private void CustomOptionsDialog_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            GlobalWindowManager.RemoveWindow(this);
+        }
+
+        /// <summary>
+        /// Execute when the add button is clicked.
+        /// </summary>
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            CustomDateOptionDialog customDateOptionDialog = new CustomDateOptionDialog();
+            UIUtil.ShowDialogAndDestroy(customDateOptionDialog);
+            if (customDateOptionDialog.DialogResult == DialogResult.OK)
+            {
+                CustomDateOptions.Add(customDateOptionDialog.CustomDateOption);
+                CustomDateOptions.Sort((x, y) => string.Compare(x.ToString(), y.ToString()));
+            }
+            CustomOptionsListBox.Items.Clear();
+            CustomDateOptions.ForEach(o => CustomOptionsListBox.Items.Add(o));
+        }
+
+        /// <summary>
+        /// Executes when the delete button is clicked.
+        /// </summary>
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            CustomDateOption option = CustomOptionsListBox.SelectedItem as CustomDateOption;
+            if (option != null)
+            {
+                CustomDateOptions.Remove(option);
+                CustomDateOptions.Sort((x, y) => string.Compare(x.ToString(), y.ToString()));
+            }
+            CustomOptionsListBox.Items.Clear();
+            CustomDateOptions.ForEach(o => CustomOptionsListBox.Items.Add(o));
+        }
+
+        /// <summary>
+        /// Close the dialog when the button is clicked.
+        /// </summary>
+        private void AcceptBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ KeePassCPEO/KeePassCPEOExt.cs
@@ -0,0 +1,191 @@
+﻿using KeePass.Forms;
+using KeePass.Plugins;
+using KeePass.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace KeePassCPEO
+{
+    /// <summary>
+    /// Plugin implementation for KeePassCPEO
+    /// </summary>
+    public class KeePassCPEOExt : Plugin
+    {
+        /// <summary>
+        /// The plugin host.
+        /// </summary>
+        private IPluginHost _host;
+
+        /// <summary>
+        /// Reference to the config menu item separator.
+        /// </summary>
+        private ToolStripSeparator _configSeparator;
+
+        /// <summary>
+        /// Reference to the config menu item.
+        /// </summary>
+        private ToolStripMenuItem _configMenuItem;
+
+        /// <summary>
+        /// The current list of custom date options.
+        /// </summary>
+        public List<CustomDateOption> CustomDateOptions { get; set; }
+
+        /// <summary>
+        /// Gets the plugin update URL.
+        /// </summary>
+        public override string UpdateUrl
+        {
+            get
+            {
+                return Properties.Resources.UpdateUrl;
+            }
+        }
+
+        /// <summary>
+        /// Icon to be displayed on the KeePass plugin list.
+        /// </summary>
+        public override Image SmallIcon
+        {
+            get
+            {
+                return Properties.Resources.Date_16x16;
+            }
+        }
+
+        /// <summary>
+        /// Build and return the path to the config file.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetConfigFilePath()
+        {
+            string configFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            configFolderPath = Path.Combine(configFolderPath, "KeePassCPEO");
+            if (!Directory.Exists(configFolderPath))
+                Directory.CreateDirectory(configFolderPath);
+            return Path.Combine(configFolderPath, "KeePassCPEO.Config.xml");
+        }
+
+        /// <summary>
+        /// Initializes the plugin.
+        /// </summary>
+        /// <param name="host">Reference to the plugin host object.</param>
+        /// <returns>True if the plugin is initialized properly, false otherwise.</returns>
+        public override bool Initialize(IPluginHost host)
+        {
+            _host = host;
+
+            // Add handler to detect when a new window has been opened.
+            GlobalWindowManager.WindowAdded += GlobalWindowManager_WindowAdded;
+
+            string configFilePath = GetConfigFilePath();
+
+            if (!File.Exists(configFilePath))
+            {
+                // Create sample dates.
+                CustomDateOptions = new List<CustomDateOption>();
+
+                CustomDateOptions.Add(new CustomDateOption { Days = 31 });
+                CustomDateOptions.Add(new CustomDateOption { Days = 45 });
+                CustomDateOptions.Add(new CustomDateOption { Days = 180 });
+            }
+            else
+            {
+                // Read config file.
+                using (FileStream fileStream = new FileStream(configFilePath, FileMode.OpenOrCreate))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<CustomDateOption>), new XmlRootAttribute("CustomDateOptions"));
+                    CustomDateOptions = serializer.Deserialize(fileStream) as List<CustomDateOption>;
+                }
+            }
+
+            // Sort custom options
+            CustomDateOptions.Sort((x, y) => string.Compare(x.ToString(), y.ToString()));
+
+            // Add configuration menu option
+            _configMenuItem = new ToolStripMenuItem("KeePassCPEO Options...");
+            _configMenuItem.Click += pluginMenuItem_Click;
+            _configMenuItem.Image = Properties.Resources.B16x16_Misc;
+
+            _configSeparator = new ToolStripSeparator();
+            _host.MainWindow.ToolsMenu.DropDownItems.Add(_configSeparator);
+            _host.MainWindow.ToolsMenu.DropDownItems.Add(_configMenuItem);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Terminates the plugin.
+        /// </summary>
+        public override void Terminate()
+        {
+            string configFilePath = GetConfigFilePath();
+
+            // Save settings.
+            using (FileStream fileStream = new FileStream(configFilePath, FileMode.Create))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<CustomDateOption>), new XmlRootAttribute("CustomDateOptions"));
+                serializer.Serialize(fileStream, CustomDateOptions);
+            }
+        }
+
+        /// <summary>
+        /// Method to handle when a new window is opened.
+        /// </summary>
+        /// <param name="sender">The sender object.</param>
+        /// <param name="e">The event arguments.</param>
+        private void GlobalWindowManager_WindowAdded(object sender, GwmWindowEventArgs e)
+        {
+            // Get the password entry form
+            PwEntryForm entryForm = e.Form as PwEntryForm;
+
+            if (entryForm != null && CustomDateOptions.Count > 0)
+            {
+                ToolStripSeparator separator = new ToolStripSeparator();
+                entryForm.DefaultTimesContextMenu.Items.Add(new ToolStripSeparator());
+
+                // Add custom date options menu items.                
+                CustomDateOptions.ForEach(o =>
+                {
+                    ToolStripMenuItem customOptionMenuItem = new ToolStripMenuItem(o.ToString());
+                    customOptionMenuItem.Tag = o;
+                    customOptionMenuItem.Click += customOptionMenuItem_Click;
+                    entryForm.DefaultTimesContextMenu.Items.Add(customOptionMenuItem);
+                });
+            }
+        }
+
+        /// <summary>
+        /// Executes whenever the user clicks on one of the custom options to set the expiration date.
+        /// </summary>
+        /// <param name="sender">The object firing the event.</param>
+        /// <param name="e">The event parameters.</param>
+        private void customOptionMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem customOptionMenuItem = sender as ToolStripMenuItem;
+            CustomDateOption option = customOptionMenuItem.Tag as CustomDateOption;
+            PwEntryForm entryForm = ((ContextMenuStrip)(((ToolStripMenuItem)sender).Owner)).SourceControl.TopLevelControl as PwEntryForm;
+
+            // Using reflection here because there is a very convenient method in PwEntryForm to use but it's set to private.
+            MethodInfo setExpireIn = entryForm.GetType().GetMethod("SetExpireIn", BindingFlags.NonPublic | BindingFlags.Instance);
+            setExpireIn.Invoke(entryForm, new object[] { option.Years, option.Months, option.Days }); // Years, Months, Days
+        }
+
+        /// <summary>
+        /// Executes whenever the user clicks on the options menu.
+        /// </summary>
+        /// <param name="sender">The object firing the event.</param>
+        /// <param name="e">The event parameters.</param>
+        private void pluginMenuItem_Click(object sender, EventArgs e)
+        {
+            CustomDateOptionsDialog optionsDialog = new CustomDateOptionsDialog(CustomDateOptions);
+            UIUtil.ShowDialogAndDestroy(optionsDialog);
+        }
+    }
+}
