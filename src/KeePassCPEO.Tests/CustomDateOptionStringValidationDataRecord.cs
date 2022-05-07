bdec86a8fe718bbd3e8fb4925210c16a78f8ccb9
Jose Quintero <jose.quintero@outlook.com>
2022-05-06 21:37:53 -0400

    Switched to data driven unit tests for checking custom date option output strings.

---
+++ src/KeePassCPEO.Tests/CustomDateOptionStringValidationDataRecord.cs
@@ -0,0 +1,18 @@
+﻿using FileHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeePassCPEO.Tests
+{
+    [DelimitedRecord("\t")]
+    public class CustomDateOptionStringValidationDataRecord
+    {
+        public int Days { get; set; }
+        public int Months { get; set; }
+        public int Years { get; set; }
+        public string ExpectedString { get; set; } = string.Empty;
+    }
+}
