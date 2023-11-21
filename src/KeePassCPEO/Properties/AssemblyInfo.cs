9d7d722a952df4995caa89c6555832f988191b85
Jose Quintero <jose.quintero@outlook.com>
2023-11-21 09:37:34 -0500

    Added GitVersion.

--- src/KeePassCPEO/Properties/AssemblyInfo.cs
+++ src/KeePassCPEO/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("KeePassCPEO")]
@@ -7,10 +6,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jose Quintero")]
 [assembly: AssemblyProduct("KeePass Plugin")]
-[assembly: AssemblyCopyright("Copyright © Jose Quintero 2022")]
+[assembly: AssemblyCopyright("Copyright © Jose Quintero 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2d8b84a2-6660-4746-baf5-2fb897769ba2")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
